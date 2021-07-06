ec87b38c95cef5256a58eab8a97619b5fe10d7c2
Gilberto Castillo <gilberto.castillo@rogers.com>
2021-07-06 11:56:21 -0400

    Update main.tf

--- us_west_1_plan/main.tf
+++ us_west_1_plan/main.tf
@@ -16,7 +16,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"
 }
 
 //--------------------------------------------------------------------
