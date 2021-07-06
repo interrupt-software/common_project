d987db4a1c3b46a75bd0b5ac78a3b240731b2aec
Gilberto Castillo <gilberto.castillo@rogers.com>
2021-07-06 11:35:44 -0400

    Update main.tf

--- us_east_1_plan/main.tf
+++ us_east_1_plan/main.tf
@@ -21,9 +21,9 @@ provider "aws" {
 
 # Create a VPC
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/24"
   tags = {
-    owner = "gilberto@hashicorp.com"
+    owner = "jeffrey@hashicorp.com"
   }
 }
 
